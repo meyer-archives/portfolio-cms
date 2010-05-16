1d908ebc685a9f2dab4f3fa2fe8876c32536871c
Mike Meyer (gmail) <github@meyer.fm>
2010-05-16 16:20:35 -0400

    Preparing for git submodules

--- system/twig-extras.php
+++ system/twig-extras.php
@@ -26,14 +26,16 @@ class Twig_Extras extends Twig_Extension{
 	public function getFilters(){
 		return array(
 			'slice' => new Twig_Filter_Function('twig_slice_filter'),
-			'first' => new Twig_Filter_Function('twig_first_filter')
+			'first' => new Twig_Filter_Function('twig_first_filter'),
+			'debug' => new Twig_Filter_Function('twig_debug_filter')
 		);
 	}
 
 	public function getTokenParsers(){
 		return array(
 			new Nav_TokenParser(),
-			new Now_TokenParser()
+			new Now_TokenParser(),
+			new Thumbnail_TokenParser()
 		);
 	}
 }
@@ -43,9 +45,11 @@ function twig_slice_filter($array, $offset, $length){
 }
 
 function twig_first_filter($array){
-	if( sizeof( $array ) > 0 )
-		return array_slice($array);
-	return array();
+	return array_shift($array);
+}
+
+function twig_debug_filter($array){
+	return "<pre>Debug output: " . print_r($array,1) . "</pre>";
 }
 
 class Nav_TokenParser extends Twig_TokenParser{
@@ -136,6 +140,42 @@ class Now_Node extends Twig_Node{
 //			->write('echo "<li class=\"nav-link\"><a href=\"".SITE_URL."\">'.$this->slug.'</a></li>"')
 
 			->write('echo date("' . $this->date_string . '");')
+
+			->raw(";\n")
+		;
+	}
+}
+
+
+class Thumbnail_TokenParser extends Twig_TokenParser{
+	public function parse(Twig_Token $token){
+		$lineno = $token->getLine();
+		$path = $this->parser->getStream()->expect(Twig_Token::STRING_TYPE)->getValue();
+		$size = $this->parser->getStream()->expect(Twig_Token::NUMBER_TYPE)->getValue();
+		$this->parser->getStream()->expect(Twig_Token::BLOCK_END_TYPE);
+
+		return new Thumbnail_Node($lineno, $path, $size, $crop_mode);
+	}
+
+	public function getTag(){
+		return 'thumbnail';
+	}
+}
+
+class Thumbnail_Node extends Twig_Node{
+	protected $date_string;
+ 
+	public function __construct($lineno, $path, $size, $crop_mode){
+		parent::__construct($lineno);
+		$this->date_string = $date_string;
+	}
+ 
+	public function compile($compiler){
+		$compiler
+			->addDebugInfo($this)
+//			->write('echo "<li class=\"nav-link\"><a href=\"".SITE_URL."\">'.$this->slug.'</a></li>"')
+
+			->write('echo ')
 
 			->raw(";\n")
 		;
